e203e055ca08650d73cfa22af19b6f9a78459f2c
Patrick Dobler <patrick.dobler@gmail.com>
2024-09-09 00:30:42 +0200

    cleanup

--- src/components/pages/OutdoorSensorPage.tsx
+++ src/components/pages/OutdoorSensorPage.tsx
@@ -1,11 +1,11 @@
-import React, { useMemo } from "react";
+import React from "react";
 import Dashboard, {
   AdditionalContextConfig,
   ChartConfig,
   OverviewCardConfig,
 } from "../dashboard/Dashboard";
 import { TimeRange } from "../../api/thingsboard-api";
-import { AggregationType, DataPointConfigs } from "../../api/data-processing";
+import { AggregationType } from "../../api/data-processing";
 import {
   faThermometerHalf,
   faWind,
@@ -30,59 +30,56 @@ interface OutdoorSensorPageProps {
 const OutdoorSensorPage: React.FC<OutdoorSensorPageProps> = ({ timeRange }) => {
   const deviceId = process.env.REACT_APP_API_DEVICE_ID_OUDOOR_SENSOR_ID;
 
-  const dataPointConfigs = useMemo<DataPointConfigs>(
-    () => ({
-      temperature: {
-        aggregationType: AggregationType.AVERAGE,
-        fractionDigits: 0,
-      },
-      humidity: {
-        aggregationType: AggregationType.AVERAGE,
-        fractionDigits: 0,
-      },
-      dewPoint: {
-        aggregationType: AggregationType.AVERAGE,
-        fractionDigits: 0,
-      },
-      windVoltageMax: {
-        aggregationType: AggregationType.MAX,
-        fractionDigits: 0,
-        valueTransformFn: (voltage: any) => (voltage / 1000) * 14,
-      },
-      windDirection: { aggregationType: AggregationType.MODE },
-      rainEventAccDifference: {
-        aggregationType: AggregationType.SUM,
-        fractionDigits: 0,
-      },
-      counter: {
-        aggregationType: AggregationType.LATEST,
-        fractionDigits: 0,
-      },
-      temperature2: {
-        aggregationType: AggregationType.LATEST,
-        fractionDigits: 0,
-      },
-      humidity2: {
-        aggregationType: AggregationType.LATEST,
-        fractionDigits: 0,
-      },
-      lux: { aggregationType: AggregationType.MAX, fractionDigits: 0 },
-      uvIndex: { aggregationType: AggregationType.MAX, fractionDigits: 0 },
-      pressure: {
-        aggregationType: AggregationType.AVERAGE,
-        fractionDigits: 0,
-      },
-      cloudBaseHeight: {
-        aggregationType: AggregationType.AVERAGE,
-        fractionDigits: 0,
-      },
-      batteryVoltage: {
-        aggregationType: AggregationType.AVERAGE,
-        fractionDigits: 0,
-      },
-    }),
-    []
-  );
+  const dataPointConfigs = {
+    temperature: {
+      aggregationType: AggregationType.AVERAGE,
+      fractionDigits: 0,
+    },
+    humidity: {
+      aggregationType: AggregationType.AVERAGE,
+      fractionDigits: 0,
+    },
+    dewPoint: {
+      aggregationType: AggregationType.AVERAGE,
+      fractionDigits: 0,
+    },
+    windVoltageMax: {
+      aggregationType: AggregationType.MAX,
+      fractionDigits: 0,
+      valueTransformFn: (voltage: any) => (voltage / 1000) * 14,
+    },
+    windDirection: { aggregationType: AggregationType.MODE },
+    rainEventAccDifference: {
+      aggregationType: AggregationType.SUM,
+      fractionDigits: 0,
+    },
+    counter: {
+      aggregationType: AggregationType.LATEST,
+      fractionDigits: 0,
+    },
+    temperature2: {
+      aggregationType: AggregationType.LATEST,
+      fractionDigits: 0,
+    },
+    humidity2: {
+      aggregationType: AggregationType.LATEST,
+      fractionDigits: 0,
+    },
+    lux: { aggregationType: AggregationType.MAX, fractionDigits: 0 },
+    uvIndex: { aggregationType: AggregationType.MAX, fractionDigits: 0 },
+    pressure: {
+      aggregationType: AggregationType.AVERAGE,
+      fractionDigits: 0,
+    },
+    cloudBaseHeight: {
+      aggregationType: AggregationType.AVERAGE,
+      fractionDigits: 0,
+    },
+    batteryVoltage: {
+      aggregationType: AggregationType.AVERAGE,
+      fractionDigits: 0,
+    },
+  };
 
   const additionalContextDataConfig: AdditionalContextConfig[] = [
     {
@@ -133,11 +130,12 @@ const OutdoorSensorPage: React.FC<OutdoorSensorPageProps> = ({ timeRange }) => {
       component: CloudBaseHeightChart,
     },
   ];
-  
-  const overviewCardConfigs: OverviewCardConfig[] = useMemo(() => [
+
+  const overviewCardConfigs: OverviewCardConfig[] = [
     {
       title: "Temperatur",
-      value: (data) => data.entries.temperature?.latestValue?.toFixed(0) ?? "--",
+      value: (data) =>
+        data.entries.temperature?.latestValue?.toFixed(0) ?? "--",
       unit: "°C",
       icon: faTemperatureLow,
       color: "#8e44ad",
@@ -151,17 +149,18 @@ const OutdoorSensorPage: React.FC<OutdoorSensorPageProps> = ({ timeRange }) => {
     },
     {
       title: "Wind",
-      value: (data) => data.entries.windVoltageMax?.latestValue?.toFixed(0) ?? "--",
+      value: (data) =>
+        data.entries.windVoltageMax?.latestValue?.toFixed(0) ?? "--",
       unit: "m/s",
       icon: faWind,
       color: "#3498db",
     },
     {
       title: "Regen",
-      value: (data) => data.entries.rainEventAccDifference.values.reduce(
-        (sum, value) => sum + value,
-        0
-      ).toString() ?? "--",
+      value: (data) =>
+        data.entries.rainEventAccDifference.values
+          .reduce((sum, value) => sum + value, 0)
+          .toString() ?? "--",
       unit: "mm",
       icon: faCloudRain,
       color: "#2ecc71",
@@ -189,29 +188,31 @@ const OutdoorSensorPage: React.FC<OutdoorSensorPageProps> = ({ timeRange }) => {
     },
     {
       title: "Cloudbase Height",
-      value: (data) => data.entries.cloudBaseHeight?.latestValue?.toFixed(0) ?? "--",
+      value: (data) =>
+        data.entries.cloudBaseHeight?.latestValue?.toFixed(0) ?? "--",
       unit: "m",
       icon: faCloud,
       color: "#7f8c8d",
     },
-  ], []);
+  ];
 
   if (!deviceId) {
     return (
-      <div>Error: REACT_APP_API_DEVICE_ID_OUDOOR_SENSOR_ID is not set in the .env file</div>
+      <div>
+        Error: REACT_APP_API_DEVICE_ID_OUDOOR_SENSOR_ID is not set in the .env
+        file
+      </div>
     );
   }
   return (
-    <>
-      <Dashboard
-        deviceId={deviceId}
-        timeRange={timeRange}
-        dataPointConfigs={dataPointConfigs}
-        additionalContextDataConfig={additionalContextDataConfig}
-        chartConfigs={chartConfigs}
-        overviewCardConfigs={overviewCardConfigs}
-      />
-    </>
+    <Dashboard
+      deviceId={deviceId}
+      timeRange={timeRange}
+      dataPointConfigs={dataPointConfigs}
+      additionalContextDataConfig={additionalContextDataConfig}
+      chartConfigs={chartConfigs}
+      overviewCardConfigs={overviewCardConfigs}
+    />
   );
 };
 
